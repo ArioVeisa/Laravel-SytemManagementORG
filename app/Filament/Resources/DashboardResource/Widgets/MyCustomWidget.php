e7b6b4bde9c4fdf61f3053c183a9fb2331c9c6c4
ArioVeisa <classlearning04g@gmail.com>
2025-01-22 23:26:13 +0700

    main

---
+++ app/Filament/Resources/DashboardResource/Widgets/MyCustomWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Resources\DashboardResource\Widgets;
+
+use Filament\Widgets\Widget;
+
+class MyCustomWidget extends Widget
+{
+    protected static string $view = 'filament.resources.dashboard-resource.widgets.my-custom-widget';
+}
