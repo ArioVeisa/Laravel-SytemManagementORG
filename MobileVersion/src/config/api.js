712ad2a0412b83a1c5592d390842912aa8967835
ArioVeisa <classlearning04@gmail.com>
2025-11-02 10:29:51 +0700

    Configure ngrok API URL and prepare for APK build

--- MobileVersion/src/config/api.js
+++ MobileVersion/src/config/api.js
@@ -1,9 +1,8 @@
-// UBAH IP INI SESUAI IP KOMPUTER ANDA!
-// Linux: hostname -I
-// Mac: ipconfig getifaddr en0
-// Windows: ipconfig (cari IPv4 Address)
-// Atau gunakan 10.0.2.2 jika testing di Android Emulator
-const API_BASE_URL = 'http://10.148.56.32:8000/api/v1';
+// Production: Gunakan ngrok URL
+const API_BASE_URL = 'https://gertrude-uncategorised-laurene.ngrok-free.dev/api/v1';
+
+// Development: Gunakan IP lokal (uncomment untuk testing lokal)
+// const API_BASE_URL = 'http://10.148.56.32:8000/api/v1';
 
 export default API_BASE_URL;
 
